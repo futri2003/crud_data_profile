641a781a01ba96831cbc312630fab812309625dd
futri nurlaili <futrinurlaili2003@gmail.com>
2022-04-06 08:59:35 +0700

    upload

---
+++ db_portopolio_futri.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2022 at 03:58 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_portopolio_futri`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_profile`
+--
+
+CREATE TABLE `tbl_profile` (
+  `id` int(11) NOT NULL,
+  `nama_depan` varchar(100) NOT NULL,
+  `nama_belakang` varchar(100) NOT NULL,
+  `tempat_lahir` varchar(100) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(5) NOT NULL,
+  `alamat` text NOT NULL,
+  `jenjang_pendidikan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_profile`
+--
+
+INSERT INTO `tbl_profile` (`id`, `nama_depan`, `nama_belakang`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `jenjang_pendidikan`) VALUES
+(2, 'Pepi', 'doang', 'Majalengka', '2004-12-22', 'P', 'selaawi rt 001 rw 008', 'D3'),
+(10, 'futri', 'nurlaili', 'tasikmalaya', '2003-10-23', 'P', 'cikanyere', 'SMK'),
+(12, 'dila', 'hissaniah', 'tasikmalaya', '2022-03-15', 'P', 'jl pelang', 'SMK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
